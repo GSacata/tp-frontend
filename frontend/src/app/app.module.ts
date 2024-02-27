39aca2ee1c036dd5549e12a2f560722e5a0d7162
GSacata <gsacata@gmail.com>
2024-02-27 00:09:23 -0300

    refined task interface

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgFor } from "@angular/common";
+import { NgFor, NgIf } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 @NgModule({
     declarations: [],
-    imports: [NgFor],
-    exports: [NgFor],
+    imports: [
+        NgFor, NgIf
+    ],
+    exports: [
+        NgFor, NgIf
+    ],
     providers: [],
     bootstrap: []
 })
